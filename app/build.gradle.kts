dd7f5fbd50f2c8b1533bcf75216683f5c11ded2a
swl16 <sweili165@gmail.com>
2026-08-29 00:45:42 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias (libs.plugins.ksp)
-    kotlin("plugin.serialization") version "1.9.0"
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
