be294d06930ecdb28e924f34c962c36c1ab74085
swl16 <sweili165@gmail.com>
2026-08-29 13:54:42 +0800

    1

--- app/src/main/java/com/example/assignment/database/SupabaseService.kt
+++ app/src/main/java/com/example/assignment/database/SupabaseService.kt
@@ -9,7 +9,7 @@ import io.github.jan.supabase.postgrest.Postgrest
 object SupabaseService {
     val client: SupabaseClient = createSupabaseClient(
         supabaseUrl = "https://fwfkoqcimgvtszucaxvp.supabase.co",
-        supabaseKey = "sb_secret_LX-D5P2bt5inL3I4FTL-yA_mJBxybxR"
+        supabaseKey = "sb_publishable_fMIDfmIRVenY0dwfyOH2vA_Fp7OUgiB"
     ) {
         install(Postgrest)
         install(Auth)
