48101a3011d3185b011ff39d45fa85eefd8a8c79
swl16 <sweili165@gmail.com>
2026-09-02 23:58:30 +0800

    1

--- app/src/main/java/com/example/assignment/database/AppDatabase.kt
+++ app/src/main/java/com/example/assignment/database/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class, Appointment::class, EmergencyContact::class, Record::class], version = 5, exportSchema = false)
+@Database(entities = [User::class, Appointment::class, EmergencyContact::class, Record::class, Reminder::class], version = 5, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
